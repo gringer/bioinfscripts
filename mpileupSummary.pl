a517cdcb819d0fd7624989943114210e65c5f39b
David Eccles (gringer) <github@gringer.org>
2014-09-30 17:24:35 +1300

    added sample name column option to aid concatenation of files

--- mpileupSummary.pl
+++ mpileupSummary.pl
@@ -4,14 +4,19 @@ use strict;
 
 use Getopt::Long qw(:config auto_help pass_through);
 
-GetOptions() or
+my $sampleName = "";
+
+GetOptions("samplename=s" => \$sampleName) or
   die("Error in command line arguments");
 
 my $assembly = "";
 
-printf("%-15s %8s %3s %8s %s\n",
+if($sampleName){
+  printf("%-15s ", "Sample");
+}
+printf("%-30s %8s %3s %8s %s\n",
        "Assembly", "Position", "Ref", "Coverage",
-       "   .   A   C   G   T   d   i");
+       "       .   A   C   G   T   d   i");
 
 while(<>){
   chomp;
@@ -19,11 +24,15 @@ while(<>){
   if(scalar(@F) <= 4){
     next;
   }
-  printf("%-15s %8d %3s %8d", $F[0], $F[1], $F[2], $F[3]);
+  if($sampleName){
+    printf("%-15s ", $sampleName);
+  }
+  printf("%-30s %8d %3s %8d", $F[0], $F[1], $F[2], $F[3]);
   splice(@F,0,4);
   $_ = $F[0];
   my @inss = m/\+[0-9]+[ACGTNacgtn]+/g;
   my $i = scalar(@inss);
+  s/\^.//g;
   s/(\+|-)[0-9]+[ACGTNacgtn]+//g;
   my $r = tr/,.//;
   my $d = tr/*//;
@@ -31,5 +40,5 @@ while(<>){
   my $c = tr/cC//;
   my $g = tr/gG//;
   my $t = tr/tT//;
-  printf("%4d %3d %3d %3d %3d %3d %3d\n", $r, $a, $c, $g, $t, $d, $i);
+  printf(" %8d %3d %3d %3d %3d %3d %3d\n", $r, $a, $c, $g, $t, $d, $i);
 }
