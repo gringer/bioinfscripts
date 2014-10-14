fab8c0a9a0c7bfaf9294eccf44b29e9b0d9344de
David Eccles (gringer) <github@gringer.org>
2014-10-14 19:17:24 +1300

    added proportion information

--- mpileupSummary.pl
+++ mpileupSummary.pl
@@ -5,8 +5,10 @@ use strict;
 use Getopt::Long qw(:config auto_help pass_through);
 
 my $sampleName = "";
+my $proportion = 0;
 
-GetOptions("samplename=s" => \$sampleName) or
+GetOptions("samplename=s" => \$sampleName,
+          "proportion!" => \$proportion) or
   die("Error in command line arguments");
 
 my $assembly = "";
@@ -14,9 +16,15 @@ my $assembly = "";
 if($sampleName){
   printf("%-15s ", "Sample");
 }
-printf("%-30s %8s %3s %8s %s\n",
-       "Assembly", "Position", "Ref", "Coverage",
-       "       .   A   C   G   T   d   i");
+if($proportion){
+  printf("%-30s %8s %3s %8s %s\n",
+         "Assembly", "Position", "Ref", "Coverage",
+         " RefP     A     C     G     T     d     i");
+} else {
+  printf("%-30s %8s %3s %8s %s\n",
+         "Assembly", "Position", "Ref", "Coverage",
+         "       .   A   C   G   T   d   i");
+}
 
 while(<>){
   chomp;
@@ -28,6 +36,8 @@ while(<>){
     printf("%-15s ", $sampleName);
   }
   printf("%-30s %8d %3s %8d", $F[0], $F[1], $F[2], $F[3]);
+  my $refAllele = $F[2];
+  my $coverage = $F[3];
   splice(@F,0,4);
   $_ = $F[0];
   my @inss = m/\+[0-9]+[ACGTNacgtn]+/g;
@@ -40,5 +50,22 @@ while(<>){
   my $c = tr/cC//;
   my $g = tr/gG//;
   my $t = tr/tT//;
-  printf(" %8d %3d %3d %3d %3d %3d %3d\n", $r, $a, $c, $g, $t, $d, $i);
+  if($proportion){
+    my $total = $i+$r+$d+$a+$c+$g+$t;
+    if($refAllele eq "A"){
+      $a = $r;
+    } elsif($refAllele eq "C"){
+      $c = $r;
+    } elsif($refAllele eq "G"){
+      $g = $r;
+    } elsif($refAllele eq "T"){
+      $t = $r;
+    }
+    ($r, $i, $d, $a, $c, $g, $t) = map {$_ * 100 / $coverage}
+      ($r, $i, $d, $a, $c, $g, $t);
+    printf(" %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n",
+           $r, $a, $c, $g, $t, $d, $i);
+  } else {
+    printf(" %8d %3d %3d %3d %3d %3d %3d\n", $r, $a, $c, $g, $t, $d, $i);
+  }
 }
