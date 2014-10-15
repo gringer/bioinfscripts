f7fe4e95f9bfe7617eac69a9853f06d4a176f247
David Eccles (gringer) <github@gringer.org>
2014-10-16 10:50:54 +1300

    Added difference threshold check

--- nanopore_barcode.pl
+++ nanopore_barcode.pl
@@ -6,7 +6,7 @@ use strict;
 use Pod::Usage; ## uses pod documentation in usage code
 use Getopt::Long qw(:config auto_version auto_help);
 
-our $VERSION = "0.1";
+our $VERSION = "0.2";
 
 =head1 NAME
 
@@ -15,7 +15,7 @@ the nanopore sequencer
 
 =head1 SYNOPSIS
 
-./nanopore_barcode.pl -length <int> -count <int>
+./nanopore_barcode.pl [options]
 
 =head2 Options
 
@@ -23,32 +23,55 @@ the nanopore sequencer
 
 =item B<-length>
 
-Length of barcode to generate (default 30)
+Length of barcode to generate (default 15)
 
 =item B<-count>
 
 Number of barcodes to generate (default 1)
 
+=item B<-threshold>
+
+Difference threshold for barcode inclusion (default 0.2)
+
 =back
 
 =head1 DESCRIPTION
 
-You will need to verify independently that the chosen barcodes are at
-least 20% different from each other.
+Note: when more than one barcode is generated, each new barcode is
+checked to make sure it is sufficiently different from previously
+added sequences.
 
 =cut
 
-my $length = 30;
+my $length = 15;
 my $count = 1;
+my $threshold = 0.2;
 
 GetOptions('length=i' => \$length,
            'count=i'=> \$count,
+           'threshold=f'=> \$threshold,
 ) or pod2usage(1);
 
 my $lastBase = "";
 
-for(my $seqNum = 1; $seqNum <= $count; $seqNum++){
-    my $sequence = "";
+sub seqsDifferent {
+  my ($seq1, $seq2, $diffThreshold) = @_;
+  my $differences = 0;
+  for(my $i = 0; $i < length($seq1); $i++){
+    if(substr($seq1,$i,1) ne substr($seq2,$i,1)){
+      $differences++;
+    }
+  }
+  my $diffProp = $differences / length($seq1);
+  return($diffProp >= $diffThreshold);
+}
+
+my @addedSeqs = ();
+my $sequence = "";
+my $seqNum = 0;
+
+while($seqNum < $count){
+    $sequence = "";
     my $lastBase = "C";
     for(my $i = 0; $i < $length; $i++){
         my @nextBaseOptions = grep {$_ ne $lastBase} ("A","T","C");
@@ -57,6 +80,11 @@ for(my $seqNum = 1; $seqNum <= $count; $seqNum++){
         $sequence .= $nextBase;
         $lastBase = $nextBase;
     }
-    printf(">Barcode_%03d\n%s\n", $seqNum, $sequence);
+    my $addedDiffCount =
+      grep {seqsDifferent($_,$sequence,$threshold)} @addedSeqs;
+    if(!@addedSeqs || ($addedDiffCount == scalar(@addedSeqs))){
+      $seqNum++;
+      printf(">Barcode_%03d\n%s\n", $seqNum, $sequence);
+      push(@addedSeqs, $sequence);
+    }
 }
-
