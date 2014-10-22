fcc0d99a9e1a6d11194398e799c2fac65bf3cb3c
David Eccles (gringer) <github@gringer.org>
2014-10-23 08:59:55 +1300

    Single line output option, remove final blank lines in sequence output

--- fastq2fasta.pl
+++ fastq2fasta.pl
@@ -5,8 +5,9 @@ use strict;
 use Getopt::Long qw(:config auto_help pass_through);
 
 my $minLength = 0;
+my $singleLine = 0;
 
-GetOptions("minLength=i" => \$minLength) or
+GetOptions("minLength=i" => \$minLength, "singleLine!" => \$singleLine) or
   die("Error in command line arguments");
 
 my $inQual = 0; # false
@@ -30,7 +31,10 @@ while(<>){
       $qual = "";
       if(length($seq) > $minLength){
         my $printedSeq = $seq;
-        $printedSeq =~ s/(.{60})/$1\n/g;
+        if(!$singleLine){
+          $printedSeq =~ s/(.{60})/$1\n/g;
+          $printedSeq =~ s/\n$//g;
+        }
         printf(">%s\n%s\n", $seqID, $printedSeq);
       }
     } else {
