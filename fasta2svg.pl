07d5c66777389e08c906568eb4e78005506b8d74
David Eccles (gringer) <github@gringer.org>
2015-10-18 00:26:49 +1300

    Update SVG header to work properly in iceweasel

--- fasta2svg.pl
+++ fasta2svg.pl
@@ -49,7 +49,8 @@ my $seqCount = 1;
 my $seqID = "";
 
 print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
-print("<svg xmlns:svg=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n");
+print("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" ".
+      "width=\"100%\" height=\"100%\" preserveAspectRatio=\"xMidYMid\">\n");
 
 
 while(<>){
