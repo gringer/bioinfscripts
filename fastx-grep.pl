63e400d437b610d9224ed6ce51fe51fe025b8720
David Eccles (gringer) <github@gringer.org>
2016-03-30 10:18:09 +1300

    Pattern search can be applied now to sequence or ID

--- fastx-grep.pl
+++ fastx-grep.pl
@@ -6,9 +6,11 @@ use Getopt::Long qw(:config auto_help pass_through);
 
 my $quiet = 0;
 my $filterSeqs = "";
+my $filterIDs = "";
 my $reverse = 0;
 
-GetOptions("filter=s" => \$filterSeqs, "reverse|v!" => \$reverse, "quiet!" => \$quiet) or
+GetOptions("filter=s" => \$filterSeqs, "idfilter=s" => \$filterIDs,
+           "reverse|v!" => \$reverse, "quiet!" => \$quiet) or
   die("Error in command line arguments");
 
 # unknown commands are treated as identifiers
@@ -23,14 +25,26 @@ while(@ARGV){
 }
 @ARGV = @files;
 
-$filterSeqs =~ s/^\|//;
-$filterSeqs = "($filterSeqs)";
-
-if($reverse){
+if($filterSeqs){
+  $filterSeqs =~ s/^\|//;
+  $filterSeqs = "($filterSeqs)";
+  if($reverse){
     printf(STDERR "Filter sequence (excluded): $filterSeqs\n");
-} else {
+  } else {
     printf(STDERR "Filter sequence: $filterSeqs\n");
+  }
 }
+
+if($filterIDs){
+  $filterIDs =~ s/^\|//;
+  $filterIDs = "($filterIDs)";
+  if($reverse){
+    printf(STDERR "Filter ID (excluded): $filterIDs\n");
+  } else {
+    printf(STDERR "Filter ID: $filterIDs\n");
+  }
+}
+
 
 my $inQual = 0; # false
 my $seqID = "";
@@ -44,7 +58,8 @@ while(<>){
     if(/^(>|@)((.+?)( .*?\s*)?)$/){
       my $newSeqID = $2;
       my $newShortID = $3;
-      if($seqID && ($reverse xor ($seq =~ /$filterSeqs/))){
+      if($seqID && (!$filterSeqs || ($reverse xor ($seq =~ /$filterSeqs/)))
+        && (!$filterIDs || ($reverse xor ($seqID =~ /$filterIDs/)))){
         if($qual){
           printf("@%s\n%s\n+\n%s\n", $seqID, $seq, $qual);
         } else {
