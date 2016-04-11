f2e90aa142ef9d5464e0072c4a87b2f68b7d564d
David Eccles (gringer) <github@gringer.org>
2016-04-12 08:36:50 +1200

    updated boundary conditions

--- chunkFilter.pl
+++ chunkFilter.pl
@@ -14,22 +14,29 @@ my @lineBuffer = ();
 while(<>){
   chomp;
   my @F = split(/ /);
+  my $newStart = $F[1];
+  my $newEnd = $F[1] + $F[2];
+  my $oldEnd = $start + $length;
   if(($F[0] eq $lastContig) &&
-     ((abs($start-$F[1]) <= $mw) || (abs($length-$F[2]) <= $mw))){
-    $F[1] = $start if $start < $F[1];
-    $F[2] = $length if $length > $F[2];
+     (($start - $mw) < $newStart && ($oldEnd + $mw) > $newStart) ||
+     (($oldEnd + $mw) > $newEnd && ($start - $mw) < $newEnd)){
+    $start = $newStart if ($start > $newStart);
+    $newEnd = $oldEnd if ($oldEnd > $newEnd);
+    $F[1] = $start;
+    $F[2] = $newEnd - $start;
   } else {
     push(@lineBuffer,"");
-    my $sig = join(".", ($lastContig, $start, $length));
-    print(join(" | $sig \n", @lineBuffer));
+    my $sig = join(" ", ($lastContig, $start, $length));
+    print(join(" $sig\n", @lineBuffer));
     @lineBuffer = ();
-    $lastContig="";
-    $start=-($mw+1);
-    $length=-($mw+1);
+    $lastContig = "";
+    $start = -($mw+1);
+    $length = -($mw+1);
   }
   push(@lineBuffer, $_);
-  ($lastContig, $start, $length)=@F;
+  ($lastContig, $start, $length) = @F;
 }
 
-my $sig = join(".", ($lastContig, $start, $length));
-print(join(" | $sig \n", @lineBuffer));
+my $sig = join(" ", ($lastContig, $start, $length));
+push(@lineBuffer,"");
+print(join(" $sig\n", @lineBuffer));
