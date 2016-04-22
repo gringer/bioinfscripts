27f2ca94913c39972ac5b482ab51ce5cfc561ca7
David Eccles (gringer) <github@gringer.org>
2016-04-22 14:08:42 +1200

    Debugging and logic pruning

--- psl_scaffolder.pl
+++ psl_scaffolder.pl
@@ -2,6 +2,7 @@
 
 use warnings;
 use strict;
+use English;
 
 use Pod::Usage; ## uses pod documentation in usage code
 use Getopt::Long qw(:config auto_version auto_help pass_through);
@@ -21,9 +22,9 @@ psl_scaffolder.pl - use self-mapped PSL file to scaffold a genome
 
 sub rc {
   my ($seq) = @_;
-  $seq =~ tr/ACGTUYRSWMKDVHBXN-/TGCARYSWKMHBDVXN-/;
+  $seq =~ tr/ACGTUYRSWMKDVHBXN-/TGCAARYSWKMHBDVXN-/;
   # work on masked sequences as well
-  $seq =~ tr/acgtuyrswmkdvhbxn/tgcaryswkmhbdvxn/;
+  $seq =~ tr/acgtuyrswmkdvhbxn/tgcaaryswkmhbdvxn/;
   return(scalar(reverse($seq)));
 }
 
@@ -36,11 +37,12 @@ sub getConsensus {
   # if different, convert to upper case to simplify lookup
   my $bc = uc(($b1 cmp $b2) ? $b1.$b2 : $b2.$b1);
   my %consensusLookup =
-    (AC => "M",
-     GT => "K",
-     AG => "R",
-     CT => "Y",
-     AT => "W");
+    (AC => "M", AM => "A", CM => "C",
+     GT => "K", GK => "G", KT => "T",
+     AG => "R", AR => "A", GR => "G",
+     CT => "Y", CY => "C", TY => "T",
+     AT => "W", AW => "A", TW => "T",
+    );
   # if "simple" ambiguity can be found, return that, otherwise return N
   # (i.e. GT => K, -A => N, YM -> N)
   return( ($consensusLookup{$bc}) ? $consensusLookup{$bc} : "N");
@@ -48,9 +50,9 @@ sub getConsensus {
 
 sub getMatch {
   my ($b1, $b2) = @_;
-  return(($b1 eq $b2) ? "*" : " ");
+  return((($b1 eq $b2) || ($b1 eq " ") || ($b2 eq " ") ||
+         ($b1 eq "N") || ($b2 eq "N")) ? " " : "*");
 }
-
 
 ############### Program starts here
 
@@ -60,9 +62,10 @@ my $projOpts =
   {
    "query" => 0, # contig file for query sequences
    "pid" => 90, # percent ID threshold
+   "trimlimit" => 50, # max number of errors outside match region
   };
 
-GetOptions($projOpts, 'query=s', 'pid=i');
+GetOptions($projOpts, 'query=s', 'pid=i', 'trimlimit=i');
 
 # process remaining command line arguments (hopefully only PSL files)
 while (@ARGV) {
@@ -147,8 +150,8 @@ while(<>){
   if(($pid >= $projOpts->{"pid"}) &&
      $querySeqs{$qName} && $targetSeqs{$tName}){
     my %meta = ();
-    my $shortTarget = ($qAliSize >= $tAliSize) ? 1 : 0;
-    my $longTarget = ($qAliSize < $tAliSize) ? 1 : 0;
+    my $shortTarget = ($tSize < $qSize) ? 1 : 0;
+    my $longTarget = (1 - $shortTarget);
     my $sName = $fields[9 + ($shortTarget * 4)];
     my $lName = $fields[9 + ($longTarget * 4)];
     my $sLen = $fields[10 + ($shortTarget * 4)];
@@ -165,69 +168,82 @@ while(<>){
     if($shortTarget){
       ($sSeq, $lSeq) = ($lSeq, $sSeq);
     }
+    my $doRC = ($strand eq "-");
+    my $preTrim = min($sStart, $lStart);
+    my $postTrim = min($sLen - $sEnd, $lLen - $lEnd);
+    ## Only continue on if there's a good likelihood that this will work
+    ## i.e. trimLength * (1-%id) < threshold
     my $sPre = substr($sSeq, 0, $sStart);
     my $lPre = substr($lSeq, 0, $lStart);
     my $sMid = substr($sSeq, $sStart, $sEnd-$sStart);
     my $lMid = substr($lSeq, $lStart, $lEnd-$lStart);
     my $sPost = substr($sSeq, $sEnd);
     my $lPost = substr($lSeq, $lEnd);
-    my $doRC = ($strand eq "-");
-    if($doRC){
-      if($shortTarget){  # target sequence is assumed to be forward strand
-        ($lPre, $lMid, $lPost) = (rc($lPost), rc($lMid), rc($lPre));
-        $lSeq = rc($lSeq);
+    if ($doRC) {
+      if ($shortTarget) { # target sequence is assumed to be forward strand
+        ($lSeq, $lPre, $lMid, $lPost) = (rc($lSeq), rc($lPost), rc($lMid), rc($lPre));
+        $preTrim = min($sStart, $lLen - $lEnd);
+        $postTrim = min($sLen - $sEnd, $lStart);
       } else {
-        ($sPre, $sMid, $sPost) = (rc($sPost), rc($sMid), rc($sPre));
-        $sSeq = rc($sSeq);
+        ($sSeq, $sPre, $sMid, $sPost) = (rc($sSeq), rc($sPost), rc($sMid), rc($sPre));
+        $preTrim = min($sLen - $sEnd, $lStart);
+        $postTrim = min($sStart, $lLen - $lEnd);
       }
     }
-    my $preLength = max(length($sPre), length($lPre));
-    printf("%s %s\n", $sName, $lName);
-    printf("%d/%d-%d %d/%d-%d\n", $sLen, $sStart, $sEnd, $lLen, $lStart, $lEnd);
-    printf("%d %d\n", length($sSeq), length($lSeq));
-    #printf("%s [%d bp]\n",
-    #      $sSeq, length($sSeq));
-    #printf("%${preLength}s %s %s [%d bp]\n",
-    #      $sPre, $sMid, $sPost, length($sPre.$sMid.$sPost));
-    printf("%s [%d bp]\n",
-           $sMid, length($sMid));
-    #printf("%s [%d bp]\n",
-    #       $lSeq, length($lSeq));
-    #printf("%${preLength}s %s %s [%d bp]\n",
-    #       $lPre, $lMid, $lPost, length($lPre.$lMid.$lPost));
-    printf("%s [%d bp]\n",
-           $lMid, length($lMid));
-    print("Blocks: \n");
-    print(join(" ",@blSizes)."\n");
-    print(join(" ",@sBlStarts)."\n");
-    print(join(" ",@lBlStarts)."\n");
-    my $lastS = $sBlStarts[0];
-    my $lastL = $lBlStarts[0];
-    my $alSeqS = "";
-    my $alSeqL = "";
-    for(my $i = 0; $i <= $#blSizes; $i++){
-      my $gapS = $sBlStarts[$i] - $lastS;
-      my $gapL = $lBlStarts[$i] - $lastL;
-      my $gapLength = max($gapS, $gapL);
-      my $fillS = $gapLength - $gapS;
-      my $fillL = $gapLength - $gapL;
-      $alSeqS .= ("-" x $fillS) . substr($sSeq, $sBlStarts[$i]-$gapS, $gapS);
-      $alSeqL .= ("-" x $fillL) . substr($lSeq, $lBlStarts[$i]-$gapL, $gapL);
-      $alSeqS .= substr($sSeq, $sBlStarts[$i], $blSizes[$i]);
-      $alSeqL .= substr($lSeq, $lBlStarts[$i], $blSizes[$i]);
-      $lastS = $sBlStarts[$i] + $blSizes[$i];
-      $lastL = $lBlStarts[$i] + $blSizes[$i];
+    if((($preTrim + $postTrim) * (101 - $pid) / 100) <
+       $projOpts->{"trimlimit"}){
+      print(STDERR "\n$sName $lName\n");
+      printf(STDERR "PreTrim: %d, PostTrim: %d, pid = %d\n",
+             $preTrim, $postTrim, $pid);
+      printf(STDERR "Short extensions: %d, %d\n",
+             length($sPre), length($sPost));
+      printf(STDERR "Long extensions: %d, %d\n",
+             length($lPre), length($lPost));
+      my $sPreTrim = substr($sPre, length($sPre)-$preTrim);
+      my $sPostTrim = substr($sPost, 0, $postTrim);
+      my $lPreTrim = substr($lPre, length($lPre)-$preTrim);
+      my $lPostTrim = substr($lPost, 0, $postTrim);
+      print(STDERR "$sPreTrim|...|$sPostTrim\n");
+      print(STDERR "$lPreTrim|...|$lPostTrim\n");
+      my $preLen = max(length($sPre), length($lPre));
+      my $postLen = max(length($sPost), length($lPost));
+      my $lastS = $sBlStarts[0];
+      my $lastL = $lBlStarts[0];
+      my $alSeqS = "";
+      my $alSeqL = "";
+      for (my $i = 0; $i <= $#blSizes; $i++) {
+        my $gapS = $sBlStarts[$i] - $lastS;
+        my $gapL = $lBlStarts[$i] - $lastL;
+        my $gapLength = max($gapS, $gapL);
+        my $fillS = $gapLength - $gapS;
+        my $fillL = $gapLength - $gapL;
+        $alSeqS .= ("-" x $fillS) . substr($sSeq, $sBlStarts[$i]-$gapS, $gapS);
+        $alSeqL .= ("-" x $fillL) . substr($lSeq, $lBlStarts[$i]-$gapL, $gapL);
+        $alSeqS .= substr($sSeq, $sBlStarts[$i], $blSizes[$i]);
+        $alSeqL .= substr($lSeq, $lBlStarts[$i], $blSizes[$i]);
+        $lastS = $sBlStarts[$i] + $blSizes[$i];
+        $lastL = $lBlStarts[$i] + $blSizes[$i];
+      }
+      #$alSeqS = sprintf("%${preLen}s",$sPre) . "|%|" . $alSeqS . "|%|" .
+      #  sprintf("%-${postLen}s",$sPost);
+      #$alSeqL = sprintf("%${preLen}s",$lPre) . "|%|" . $alSeqL . "|%|" .
+      #  sprintf("%-${postLen}s",$lPost);
+      $alSeqS = sprintf("%${preTrim}s",$sPreTrim) . "|%|" . $alSeqS . "|%|" .
+        sprintf("%-${postTrim}s",$sPostTrim);
+      $alSeqL = sprintf("%${preTrim}s",$lPreTrim) . "|%|" . $alSeqL . "|%|" .
+        sprintf("%-${postTrim}s",$lPostTrim);
+      my $alConsensus = "";
+      my $alMatch = "";
+      for (my $i = 0; $i < length($alSeqS); $i++) {
+        $alConsensus .= getConsensus(substr($alSeqS,$i,1),substr($alSeqL,$i,1));
+        $alMatch .= getMatch(substr($alSeqS,$i,1),substr($alSeqL,$i,1));
+      }
+      print(STDERR "&----&\n");
+      print(STDERR $alSeqS."\n");
+      print(STDERR $alConsensus."\n");
+      print(STDERR $alMatch."\n");
+      print(STDERR $alSeqL."\n");
     }
-    my $alConsensus = "";
-    my $alMatch = "";
-    for(my $i = 0; $i < length($alSeqS); $i++){
-      $alConsensus .= getConsensus(substr($alSeqS,$i,1),substr($alSeqL,$i,1));
-      $alMatch .= getMatch(substr($alSeqS,$i,1),substr($alSeqL,$i,1));
-    }
-    print($alSeqS."\n");
-    print($alConsensus."\n");
-    print($alMatch."\n");
-    print($alSeqL."\n");
   }
   #printf("%0.1f\n", $pid);
 }
