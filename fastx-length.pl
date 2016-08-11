b46e0cf75dbb51a5f76b7c51f3d74201554ab2d9
David Eccles (gringer) <github@gringer.org>
2016-08-11 18:08:34 +1200

    Fixes for indexing, now works better with 1 or 2 sequences

--- fastx-length.pl
+++ fastx-length.pl
@@ -59,16 +59,14 @@ if($seqID){
 @lengths = sort {$b <=> $a} (@lengths);
 my $sum = 0;
 my @cumLengths = map {$sum += $_} (@lengths);
-my $L50Length = $sum * 0.5;
-my @L50cumLengths = grep {$_ < $L50Length} @cumLengths;
-my $L50LengthNum = $#L50cumLengths;
-if($L50cumLengths[$L50LengthNum] < $L50Length){
+
+my $L50LengthNum = 0;
+while($cumLengths[$L50LengthNum] < ($sum * 0.5)){
   $L50LengthNum++;
 }
-my $L90Length = $sum * 0.9;
-my @L90cumLengths = grep {$_ < $L90Length} @cumLengths;
-my $L90LengthNum = $#L90cumLengths;
-if($L90cumLengths[$L90LengthNum] < $L90Length){
+
+my $L90LengthNum = 0;
+while($cumLengths[$L90LengthNum] < ($sum * 0.9)){
   $L90LengthNum++;
 }
 
@@ -77,6 +75,6 @@ printf(STDERR "Total length: %sbp\n", SIConvert($sum));
 printf(STDERR "Longest sequence: %sbp\n", SIConvert($lengths[0]));
 printf(STDERR "Shortest sequence: %sbp\n", SIConvert($lengths[$#lengths]));
 printf(STDERR "N50: %d sequences; L50: %sbp\n",
-     $L50LengthNum, SIConvert($lengths[$L50LengthNum]));
+     ($L50LengthNum+1), SIConvert($cumLengths[$L50LengthNum]));
 printf(STDERR "N90: %d sequences; L90: %sbp\n",
-     $L90LengthNum, SIConvert($lengths[$L90LengthNum]));
+     ($L90LengthNum+1), SIConvert($cumLengths[$L90LengthNum]));
