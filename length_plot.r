55aff644f7451f76099749fcd67acde2d54bfd51
David Eccles (gringer) <github@gringer.org>
2016-12-14 13:45:36 +1300

    Updated graphs to add in density plots

--- length_plot.r
+++ length_plot.r
@@ -7,7 +7,9 @@ if(length(fileNames) == 0){
 }
 
 dens.mat <- sapply(fileNames, function(x){
-    data <- scan(x, comment.char=" ");
+    cat(x,"...");
+    data <- scan(x, comment.char=" ", quiet=TRUE);
+    cat(" done\n");
     res <- density(log10(data), from=2, to=6);
     res.out <- res$y;
     names(res.out) <- round(res$x,3);
@@ -16,8 +18,6 @@ dens.mat <- sapply(fileNames, function(x){
 colnames(dens.mat) <- sub("lengths_(.*)\\.txt(\\.gz)?","\\1",colnames(dens.mat));
 
 bpdens.mat <- dens.mat * 10^as.numeric(rownames(dens.mat));
-
-print(str(dens.mat));
 
 {
     png("MinION_Bases_DigElec_white.png", pointsize=24,
@@ -95,18 +95,17 @@ print(str(dens.mat));
     dummy <- dev.off();
 }
 
-
 {
     png("MinION_Bases_Cumulative.png", pointsize=24,
         width=1600, height=960);
-    par(mgp=c(4,1,0));
+    par(mgp=c(4,1,0), mar=c(5.5,5.5,0.5,0.5));
     plot(NA, xlim=range(as.numeric(rownames(bpdens.mat))), ylim=c(0,1),
          type="l", xaxt="n", xlab = "Read Length",
          ylab = "");
     for(col in seq_len(ncol(bpdens.mat))){
         points(as.numeric(rownames(bpdens.mat)),1-cumsum(bpdens.mat[,col]) /
                    sum(bpdens.mat[,col]), type="l",
-               col=hcl(h=col/ncol(bpdens.mat)*360, l=70, c=80));
+               col=hcl(h=col/ncol(bpdens.mat)*360, l=70, c=80), lwd=3);
     }
     legend("topright", legend=colnames(bpdens.mat),
            fill=hcl(h=(1:ncol(bpdens.mat))/ncol(bpdens.mat)*360, l=70, c=80),
@@ -121,14 +120,14 @@ print(str(dens.mat));
 {
     png("MinION_Reads_Cumulative.png", pointsize=24,
         width=1600, height=960);
-    par(mgp=c(4,1,0));
+    par(mgp=c(4,1,0), mar=c(5.5,5.5,0.5,0.5));
     plot(NA, xlim=range(as.numeric(rownames(dens.mat))), ylim=c(0,1),
          type="l", xaxt="n", xlab = "Read Length",
          ylab = "");
     for(col in seq_len(ncol(dens.mat))){
         points(as.numeric(rownames(dens.mat)),1-cumsum(dens.mat[,col]) /
                    sum(dens.mat[,col]), type="l",
-               col=hcl(h=col/ncol(dens.mat)*360, l=70, c=80));
+               col=hcl(h=col/ncol(dens.mat)*360, l=70, c=80), lwd=3);
     }
     legend("topright", legend=colnames(dens.mat),
            fill=hcl(h=(1:ncol(dens.mat))/ncol(dens.mat)*360, l=70, c=80),
@@ -140,6 +139,50 @@ print(str(dens.mat));
     dummy <- dev.off();
 }
 
+{
+    png("MinION_Reads_Density.png", pointsize=24,
+        width=1600, height=960);
+    par(mgp=c(4,1,0));
+    ymax <- max(dens.mat);
+    plot(NA, xlim=range(as.numeric(rownames(dens.mat))), ylim=c(0,ymax),
+         type="l", xaxt="n", xlab = "Read Length",
+         ylab = "");
+    for(col in seq_len(ncol(dens.mat))){
+        points(as.numeric(rownames(dens.mat)),dens.mat[,col], type="l",
+               col=hcl(h=col/ncol(dens.mat)*360, l=70, c=80));
+    }
+    legend("topright", legend=colnames(dens.mat),
+           fill=hcl(h=(1:ncol(dens.mat))/ncol(dens.mat)*360, l=70, c=80),
+           inset=0.05);
+    mtext("Read Density",2,3);
+    axis(1,at=log10(c(1,2,5)) + rep(0:5, each=3), las=2,
+         labels=paste0(c(1,2,5),
+             rep(substring("00000",first=0,last=0:5),each=3)));
+    dummy <- dev.off();
+}
+
+{
+    png("MinION_Bases_Density.png", pointsize=24,
+        width=1600, height=960);
+    ymax <- max(bpdens.mat / colSums(bpdens.mat));
+    par(mgp=c(4,1,0));
+    plot(NA, xlim=range(as.numeric(rownames(bpdens.mat))), ylim=c(0,ymax),
+         type="l", xaxt="n", xlab = "Read Length",
+         ylab = "");
+    for(col in seq_len(ncol(bpdens.mat))){
+        points(as.numeric(rownames(bpdens.mat)),bpdens.mat[,col] /
+                   sum(bpdens.mat[,col]), type="l",
+               col=hcl(h=col/ncol(bpdens.mat)*360, l=70, c=80));
+    }
+    legend("topright", legend=colnames(bpdens.mat),
+           fill=hcl(h=(1:ncol(bpdens.mat))/ncol(bpdens.mat)*360, l=70, c=80),
+           inset=0.05);
+    mtext("Base Density",2,3);
+    axis(1,at=log10(c(1,2,5)) + rep(0:5, each=3), las=2,
+         labels=paste0(c(1,2,5),
+             rep(substring("00000",first=0,last=0:5),each=3)));
+    dummy <- dev.off();
+}
 
 
 {
