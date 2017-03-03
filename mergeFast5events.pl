da4e9a25b7ec70cd4083781dd222f4211450f015
David Eccles (gringer) <github@gringer.org>
2017-03-03 14:31:08 +1300

    Use column names to allow for order changes in FAST5 event output (e.g. MinKNOW 1.3.25)

--- mergeFast5events.pl
+++ mergeFast5events.pl
@@ -5,32 +5,43 @@ use strict;
 
 my $model = "";
 my $cumLength = 0;
+my $cumTotalLength = 0;
 my $meanSig = 0;
 my $bpPos = 0;
 my $lastMove = 0;
 my $lastStart = 0;
 
+my %colnums = ();
+
 while(<>){
   chomp;
   my @F = split(",", $_);
   if($F[5] !~ /[0-9]/){
-    print("channel,mux,read,model_state,move,bppos,mean,start,length\n");
+    my $colNum = 0;
+    grep {$colnums{$_} = $colNum++} @F;
+    print("channel,mux,read,model_state,move,bppos,mean,start,startSecs,length\n");
     next;
   }
   ## convert start/length to samples, assuming 4kHz sampling
-  @F[(5,7)] = map({$_ * 4000} @F[(5,7)]);
-  if($F[8] ne $model){
+  @F[($colnums{start},$colnums{length})] =
+    map({$_ * $F[$colnums{sampleRate}]} @F[($colnums{start},$colnums{length})]);
+  if($F[$colnums{model_state}] ne $model){
     if($model){
-      print(join(",",(@F[(1,2,3)],$model,$lastMove,$bpPos,$meanSig,$lastStart,$cumLength)),"\n");
+      print(join(",",(@F[($colnums{runID},$colnums{channel},$colnums{mux})],
+                      $model,$lastMove,$bpPos,sprintf('%0.2f',$meanSig),
+                      sprintf('%.0f', $lastStart),
+                      sprintf('%0.4f', $lastStart/$F[$colnums{sampleRate}]),
+                      sprintf('%.0f',$cumLength))),"\n");
     }
-    $model = $F[8];
-    $lastMove = $F[9];
-    $lastStart = $F[5];
-    $bpPos += $F[9];
+    $model = $F[$colnums{model_state}];
+    $lastMove = $F[$colnums{move}];
+    $lastStart = $F[$colnums{start}] - $F[$colnums{rawStart}];
+    $bpPos += $F[$colnums{move}];
     $cumLength = 0;
     $meanSig = 0;
   }
-  $meanSig = ($meanSig * $cumLength + $F[4] * $F[7]) / ($cumLength + $F[7]);
-  $cumLength = $cumLength + $F[7];
-  ##print(join(";",@F[(1,2,3,8,9,4,5,7)]),"\n");
+  $meanSig = ($meanSig * $cumLength + $F[$colnums{mean}] * $F[$colnums{length}]) / ($cumLength + $F[$colnums{length}]);
+  $cumLength = $cumLength + $F[$colnums{length}];
+  $cumLength = int($cumLength);
+  $cumTotalLength += $cumLength;
 }
