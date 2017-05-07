27c8c53cc7b0d17625682af0175a669a5d2b9d7b
David Eccles (gringer) <bioinformatics@gringene.org>
2017-05-07 01:19:44 +0100

    Added fastx->fastm conversion script

---
+++ fastx2fastm.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $inQual = 0; # false
+my $seqID = "";
+my $qualID = "";
+my $seq = "";
+my $qual = "";
+while(<>){
+  chomp;
+  chomp;
+  if(!$inQual){
+    if(/^(>|@)((.+?)( .*?\s*)?)$/){
+      my $newSeqID = $2;
+      printf("#seq:%s", $seqID);
+      printf(" %s", $seq);
+      if($qual){
+	printf("#qual:%s", $seqID);
+	printf(" %s", $qual);
+      }
+      $seq = "";
+      $qual = "";
+      $seqID = $newSeqID;
+    } elsif(/^\+(.*)$/) {
+      $inQual = 1; # true
+      $qualID = $1;
+    } else {
+      $seq .= $_;
+    }
+  } else {
+    $qual .= $_;
+    if(length($qual) >= length($seq)){
+      $inQual = 0; # false
+    }
+  }
+}
+
+if($seqID){
+  printf("#seq:%s", $seqID);
+  printf(" %s", $seq);
+  if($qual){
+    printf("#qual:%s", $seqID);
+    printf(" %s", $qual);
+  }
+}
