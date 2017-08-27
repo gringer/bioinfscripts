5cdf4b5e61a4ff62f7be485a0866a6de772c012e
David Eccles (gringer) <github@gringer.org>
2017-08-28 10:01:41 +1200

    update for most recent readstomper output

--- posAggregate.pl
+++ posAggregate.pl
@@ -6,29 +6,44 @@ use warnings;
 use strict;
 
 my ($oldquery, $oldtarget, $olddir, $oldqs, $oldqe, $oldqml,
-    $oldql, $oldts, $oldtml, $oldtl) =
-  ("") x 10;
+    $oldql, $oldqpct, $oldts, $oldte, $oldtml, $oldtl, $oldtpct) =
+  ("") x 13;
 
 while(<>){
   chomp;
-  my ($query, $target, $dir, $qs, $qe, $qml, $ql, $ts, $tml, $tl) =
-    split(/,/);
+  my ($query, $target, $dir, $qs, $qe, $qml,
+    $ql, $qpct, $ts, $te, $tml, $tl, $tpct) =
+      split(/,/);
+  if(/^query/){
+    print($_."\n");
+    next;
+  }
   if(($query eq $oldquery) && ($target eq $oldtarget) &&
-     ($olddir eq $dir) && ($oldts <= $ts)){
+     ($olddir eq $dir) &&
+     ((($dir eq "+") && ($oldts <= $ts)) ||
+      (($dir eq "-") && ($oldte >= $te)))){
     $qs = $oldqs;
     $qml += $oldqml;
-    $ts = $oldts;
+    $qpct = (($oldqpct * $oldqml) + ($qpct * $qml)) / ($oldqml + $qml);
+    $ts = $oldts if ($dir eq "+");
+    $te = $oldte if ($dir eq "-");
     $tml += $oldtml;
+    $tpct = (($oldtpct * $oldtml) + ($tpct * $tml)) / ($oldtml + $tml);
   } elsif($oldquery ne "") {
-    printf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
-           $oldquery, $oldtarget, $olddir, $oldqs, $oldqe, $oldqml,
-           $oldql, $oldts, $oldtml, $oldtl);
+    $qpct = sprintf("%0.2f", $qpct);
+    $tpct = sprintf("%0.2f", $tpct);
+    print(join(",",($oldquery, $oldtarget, $olddir,
+                    $oldqs, $oldqe, $oldqml, $oldql, $oldqpct,
+                    $oldts, $oldte, $oldtml, $oldtl, $oldtpct))."\n");
   }
   ($oldquery, $oldtarget, $olddir, $oldqs, $oldqe, $oldqml,
-   $oldql, $oldts, $oldtml, $oldtl) =
-     ($query, $target, $dir, $qs, $qe, $qml, $ql, $ts, $tml, $tl);
+   $oldql, $oldqpct, $oldts, $oldte, $oldtml, $oldtl, $oldtpct) =
+     ($query, $target, $dir, $qs, $qe, $qml,
+      $ql, $qpct, $ts, $te, $tml, $tl, $tpct);
 }
 
-printf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
-       $oldquery, $oldtarget, $olddir, $oldqs, $oldqe, $oldqml,
-       $oldql, $oldts, $oldtml, $oldtl);
+$oldqpct = sprintf("%0.2f", $oldqpct);
+$oldtpct = sprintf("%0.2f", $oldtpct);
+print(join(",",($oldquery, $oldtarget, $olddir,
+                $oldqs, $oldqe, $oldqml, $oldql, $oldqpct,
+                $oldts, $oldte, $oldtml, $oldtl, $oldtpct))."\n");
