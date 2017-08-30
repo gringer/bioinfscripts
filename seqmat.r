342259ddca003ba498cd5f203abd1aa09ca29c61
David Eccles (gringer) <github@gringer.org>
2017-08-30 14:24:46 +1200

    Add generic command line parser, custom sequence matrix sizes

--- seqmat.r
+++ seqmat.r
@@ -1,22 +1,101 @@
 #!/usr/bin/Rscript
 
-fileName <- commandArgs(TRUE)[1];
-rptSize <- as.numeric(commandArgs(TRUE)[2]);
+argLoc <- 1;
+
+usage <- function(){
+  cat("usage: ./seqmat.r <fasta file> <repeat unit size> [options]\n");
+  cat("\nOther Options:\n");
+  cat("-help               : Only display this help message\n");
+  cat("-type (png/pdf)     : Image type (default 'png')\n");
+  cat("-size <x>x<y>       : Image size (default 1200x1200 for png, 12x12 for PDF)\n");
+  cat("-col (ef|cb)     : Colour palette (electrophoresis, colour-blind [default])\n");
+ cat("\n");
+}
+
+fileName <- "";
+rptSize <- -1;
+type <- "png";
+sizeX <- -1;
+sizeY <- -1;
+colType <- "cb";
+
+if(length(commandArgs(TRUE)) == 0){
+      usage();
+      quit(save = "no", status=0);
+}
+
+while(!is.na(commandArgs(TRUE)[argLoc])){
+    arg <- commandArgs(TRUE)[argLoc];
+    argLoc <- argLoc + 1;
+    if(arg == "-help"){
+      usage();
+      quit(save = "no", status=0);
+    } else if(arg == "-size"){
+        arg <- unlist(strsplit(commandArgs(TRUE)[argLoc], "x"));
+        argLoc <- argLoc + 1;
+        sizeX <- as.numeric(arg[1]);
+        sizeY <- as.numeric(arg[2]);
+    } else if(arg == "-type"){
+        arg <- commandArgs(TRUE)[argLoc];
+        argLoc <- argLoc + 1;
+        type <- arg;
+    } else if(arg == "-col"){
+        arg <- commandArgs(TRUE)[argLoc];
+        argLoc <- argLoc + 1;
+        colType <- arg;
+    } else {
+        if(file.exists(arg)){
+            fileName <- arg;
+        } else if ((rptSize == -1) && grepl("^[0-9]+$", arg)){
+            rptSize <- as.numeric(arg);
+        } else {
+            cat("Error: Argument '", arg,
+                "' is not understood by this program\n\n", sep="");
+            usage();
+            quit(save = "no", status=0);
+        }
+    }
+}
+
+if(rptSize == -1){
+    cat("Error: repeat unit size has not been specified\n\n");
+    usage();
+    quit(save = "no", status=0);
+}
+
 
 inLines <- readLines(fileName);
 
 inName <- substring(inLines[1],2);
 inSeq <- c(A=1, C=2, G=3, T=4)[unlist(strsplit(paste(inLines[-1],collapse=""),""))];
 
-type <- "png";
-if(any(commandArgs(TRUE) == "pdf")){
-    type <- "pdf";
+if(type == "png"){
+    if(sizeX == -1){
+        sizeX <- 1200;
+    }
+    if(sizeY == -1){
+        sizeY <- 1200;
+    }
+} else if(type == "pdf"){
+    if(sizeX == -1){
+        sizeX <- 12;
+    }
+    if(sizeY == -1){
+        sizeY <- 12;
+    }
 }
 
+colPal <-
+    if(colType == "cb"){
+        c("#006400","#0000FF","#FFD700","#FF6347","#D3D3D3");
+    } else {
+        c("#006400","#0000FF","#FFD700","#8B0000","#D3D3D3");
+    }
+
 if(type == "png"){
-    png("sequence_matrix.png", width=1200, height=1200, pointsize=24);
+    png("sequence_matrix.png", width=sizeX, height=sizeY, pointsize=24);
 } else if(type == "pdf"){
-    pdf("sequence_matrix.pdf", width=12, height=12, pointsize=16);
+    pdf("sequence_matrix.pdf", width=sizeX, height=sizeY, pointsize=16);
 }
 lis <- length(inSeq);
 numLines <- floor(lis/rptSize + 1);
@@ -27,13 +106,13 @@ image(x=1:rptSize, y=1:numLines-1, matrix(subSeq,nrow=rptSize),
       main=sprintf("%s (%0.3f kb, %d bases / line)", sub(" .*$","",inName),
                    lis/1000, rptSize), ylab="Base location",
       cex.main=0.8, xaxt="n", yaxt="n", useRaster=TRUE,
-      col=c("#006400","#0000FF","#FFD700","#FF6347","lightgrey"));
+      col=colPal);
 axis(2, at=round(seq(0, numLines, length.out=20)),
      labels=round(seq(0, numLines, length.out=20)) * rptSize+1,
      las=2, cex.axis=1);
 dummy <- dev.off();
 
-numLoops <- (length(inSeq) / rptSize);
+numLoops <- (length(subSeq) / rptSize);
 ##startCount <- rptSize / 1.2;
 startCount <- rptSize;
 startRadius <- ifelse(numLoops < 10, 0.5, 0.2);
@@ -42,9 +121,9 @@ endRadius <- 1.0;
 
 
 if(type == "png"){
-    png("sequence_circle.png", width=1200, height=1200, pointsize=24);
+    png("sequence_circle.png", width=max(sizeX,sizeY), height=max(sizeX,sizeY), pointsize=24);
 } else if(type == "pdf"){
-    pdf("sequence_circle.pdf", width=12, height=12, pointsize=16);
+    pdf("sequence_circle.pdf", width=max(sizeX,sizeY), height=max(sizeX,sizeY), pointsize=16);
 }
 par(mar=c(3,3,3,3));
 plot(NA, xlim=c(-1,1), ylim=c(-1,1), axes=FALSE, ann=FALSE);
@@ -54,10 +133,10 @@ mtext(sprintf("%s (%0.3f kb, %d bases / ring)", sub(" .*$","",inName),
 ## integrate(2*pi*r,r=startRadius..endRadius)
 ## => pi((endRadius)²-(startRadius)²)
 dTot <- pi*(endRadius^2 - startRadius^2); ## total "distance" travelled
-theta <- seq(0, numLoops * 2*pi, length.out=length(inSeq)); ## traversed angle
+theta <- seq(0, (numLoops) * 2*pi, length.out=length(subSeq)); ## traversed angle
 deg <- (theta / (2*pi)) * 360;
 r <- seq(sqrt(startRadius), sqrt(endRadius),
-         length.out=length(inSeq))^2; ## path radius
+         length.out=length(subSeq))^2; ## path radius
 s <- pi * (r^2 - startRadius^2); ## traversed distance at each pos
 ds <- c(s[2],diff(s)); ## distance difference at each pos
 
@@ -69,9 +148,9 @@ ds <- c(s[2],diff(s)); ## distance difference at each pos
 
 ## draw the spiral
 points(rev(r) * cos(rev(theta)), rev(r) * sin(rev(theta)),
-       col=c("#00640080","#0000FF80","#FFD70080","#FF634780")[rev(inSeq)],
+       col=paste0(colPal[rev(subSeq)],"80"),
        pch=16, cex=rev(sqrt(r)) * (7/log(numLoops)));
 legend("center", legend=c("A","C","G","T"), inset=0.2,
-       fill=c("#006400","#0000FF","#FFD700","#FF6347"),
+       fill=colPal[1:4],
        cex=ifelse(numLoops < 10, 1, 0.71));
 invisible(dev.off());
