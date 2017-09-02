69c2ac0ea2143a9eb5a53b4e5387427acae67fe0
David Eccles (gringer) <bioinformatics@gringene.org>
2017-09-03 08:37:58 +1200

    Filter out unlikely repetitive signals

--- fourievar.r
+++ fourievar.r
@@ -1,17 +1,24 @@
 #!/usr/bin/Rscript
 
-library(Biostrings);
-library(bitops);
-library(caTools);
+if(length(commandArgs(TRUE)) < 1){
+    cat("Error: no fasta file specified\n");
+    cat("syntax: ./fouriervar.r <input.fa>\n");
+    quit(save="no");
+}
 
-seqs <- readDNAStringSet("tig00022132_rpt0.fa")[[1]];
+library(Biostrings, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
+library(bitops, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
+library(caTools, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
+
+fileName <- commandArgs(TRUE)[1];
+seqs <- readDNAStringSet(fileName)[[1]];
 slen <- length(seqs);
 
 baseBin <- sapply(c("A","C","G","T"), function(x){(as.vector(seqs) == x)+0});
 basePoss <-
     sapply(c("A","C","G","T"), function(x){which(as.vector(seqs) == x)});
 
-fLimit <- 300;
+fLimit <- 600;
 
 spectrum <- matrix(0, slen, fLimit);
 colnames(spectrum) <- 1:ncol(spectrum);
@@ -26,9 +33,20 @@ for(cycle in 1:ncol(spectrum)){
     }
 }
 
+## Remove transient repeats
+spectrum[spectrum < 8] <- 0;
+
+##filtSpec <- apply(spectrum[10:11,], 1, function(x){
+##    rx <- rle(x);
+##    rs$values[rs$values < 10];
+##    rs$values[rs$lengths == 1];
+##    print(rle(x));
+##});
+
 smoothSpec <- apply(spectrum, 2, runmean, 20);
 
-png("DNA_spectrogram.png", height=1800, width=1800, pointsize=36);
+png(paste0("DNA_spectrogram_",sub("\\.[^.]$","",fileName),".png"),
+    height=1800, width=1800, pointsize=36);
 image(x=1:length(seqs), y=1:ncol(spectrum), spectrum,
       col=(colorRampPalette(c("white",blues9)))(100),
       xlab="Sequence location (bp)",
