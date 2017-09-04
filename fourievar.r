c8ea665bc7244b644ac9b571069c7418e9d9c1d2
David Eccles (gringer) <bioinformatics@gringene.org>
2017-09-05 09:21:51 +1200

    Change to complex number representation

--- fourievar.r
+++ fourievar.r
@@ -7,60 +7,59 @@ if(length(commandArgs(TRUE)) < 1){
 }
 
 library(Biostrings, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
-library(bitops, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
-library(caTools, quietly=TRUE, warn.conflicts=FALSE, verbose=FALSE);
 
 fuzz <- 2;
+doPlot <- FALSE;
 
+#fileName <- "/bioinf/presentations/2017-Sep-03/tig00022132_rpt0.fa";
 fileName <- commandArgs(TRUE)[1];
-seqs <- readDNAStringSet(fileName)[[1]];
+seqs <- as.vector(readDNAStringSet(fileName)[[1]]);
 slen <- length(seqs);
 fLimit <- 1000;
+fLimit <- min(slen-1, fLimit);
 
-baseBin <- sapply(c("A","C","G","T"), function(x){c((as.vector(seqs) == x)+0,rep(0,fuzz))});
-basePoss <-
-    sapply(c("A","C","G","T"), function(x){
-        res <- which(as.vector(seqs) == x);
-    });
+baseCplx <- complex(real=c(A=1, C=0, G=-1, T=0)[seqs],
+                    imaginary=c(A=0, C=1, G=0, T=-1)[seqs]);
 
 spectrum <- matrix(0, slen, fLimit);
-for(base in c("A","C","G","T")){
-    cat("Base:",base,"\n");
-    possB <- basePoss[[base]];
-    poss <- possB;
-    for(cycle in 1:ncol(spectrum)){
-        poss <- possB[possB > (cycle + fuzz)] - cycle - fuzz;
-        spectrum[poss, cycle] <-
-            baseBin[poss + 0, base] *  1 +
-            baseBin[poss + 1, base] *  4 +
-            baseBin[poss + 2, base] * 16 +
-            baseBin[poss + 3, base] *  4 +
-            baseBin[poss + 4, base] *  1;
-    }
+for(cycle in 2:fLimit){
+    spectrum[1:(slen-cycle), cycle] <-
+        (head(baseCplx, -cycle) == tail(baseCplx, -cycle)) + 0;
 }
 
 colnames(spectrum) <- 1:ncol(spectrum);
 
-cat("Removing noise\n");
-## Remove transient repeats
-spectrum[spectrum < 5] <- 0;
-binSpec <- sign(spectrum);
-filtSpec <- apply(binSpec, 2, function(x){
-    rx <- rle(x);
-    rx$values[rx$lengths <= (fuzz*3)] <- 0;
-    rx$values <- (rx$values == 1);
-    inverse.rle(rx);
-});
+## cat("Removing noise\n");
+## ## Remove transient repeats
+## spectrum[spectrum < 4] <- 0;
+## binSpec <- sign(spectrum);
+## filtSpec <- apply(binSpec, 2, function(x){
+##     rx <- rle(x);
+##     rx$values[rx$lengths <= (fuzz*3)] <- 0;
+##     rx$values <- (rx$values == 1);
+##     inverse.rle(rx);
+## });
 
-spectrum[!filtSpec] <- 0;
+## spectrum[!filtSpec] <- 0;
+## spectrum[,c(1:2)] <- NA;
 
-cat("Writing to file\n");
+scols <- colSums(sign(spectrum)) / (slen - 1:ncol(spectrum));
+cat(fileName, which(scols == max(scols, na.rm=TRUE)), max(scols, na.rm=TRUE),
+    median(scols, na.rm=TRUE), mad(scols, na.rm=TRUE), "\n");
 
-pdf(paste0("DNA_spectrogram_",sub("\\.[^\\.]$","",fileName),".pdf"),
-    height=8, width=11);
-image(x=1:length(seqs), y=1:ncol(spectrum), spectrum,
-      col=(colorRampPalette(c("white",blues9)))(100),
-      xlab="Sequence location (bp)",
-      ylab="Repeat width (bp)", useRaster=TRUE);
-abline(h=seq(0,fLimit, by=10), lty="dashed", lwd=1, col="#FF5000A0");
-invisible(dev.off());
+scols[scols==0] <- NA;
+
+if(doPlot){
+    cat("Writing to file\n");
+    pdf(paste0("DNA_spectrogram_",sub("\\.[^\\.]$","",fileName),".pdf"),
+        height=8, width=11);
+                                        #pdf(paste0("DNA_spectrogram_test.pdf"),
+                                        #    height=8, width=11);
+    image(x=1:length(seqs), y=1:ncol(spectrum), spectrum,
+          col=(colorRampPalette(c("white",blues9)))(100),
+          xlab="Sequence location (bp)",
+          ylab="Repeat width (bp)", useRaster=TRUE);
+    plot(x=1:ncol(spectrum), scols);
+    abline(h=median(scols, na.rm=TRUE)+(mad(scols, na.rm=TRUE)*3*c(-1,1)));
+    invisible(dev.off());
+}
