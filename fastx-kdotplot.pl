fc23666bd95662e394ef776e2ef3a3edd15cb2a0
David Eccles (gringer) <bioinformatics@gringene.org>
2018-01-28 23:39:37 +1300

    Alternative dotplot image, emphasises distance between features

--- fastx-kdotplot.pl
+++ fastx-kdotplot.pl
@@ -28,9 +28,10 @@ my $size = 1024;
 my $subseq = "";
 my @region = ();
 my $kmerLength = 17; ## number of bases in hash keys
+my $blockPicture = 0; ## false
 
 GetOptions("kmer=i" => \$kmerLength, "size=i" => \$size,
-           "region=s" => \$subseq ) or
+           "region=s" => \$subseq, "altview!" => \$blockPicture ) or
     die("Error in command line arguments");
 
 if($subseq){
@@ -52,9 +53,12 @@ my $black = $im->colorAllocate(0,0,0);
 my $red = $im->colorAllocate(0x8b,0,0);
 my $green = $im->colorAllocate(0,0xA0,0);
 my $blue = $im->colorAllocate(0,0,255);
-my $yellow = $im->colorAllocate(255,255,0);
-my $magenta = $im->colorAllocate(255,0,255);
-my $cyan = $im->colorAllocate(0,255,255);
+my $darkRed = $im->colorAllocate(0x6b,0,0);
+my $darkGreen = $im->colorAllocate(0,0x80,0);
+my $darkBlue = $im->colorAllocate(0,0,0xa0);
+my $yellow = $im->colorAllocate(0xa0,0x90,0);
+my $magenta = $im->colorAllocate(0x90,0,0xa0);
+my $cyan = $im->colorAllocate(0,0xa0,0x90);
 
 while(<>){
   chomp;
@@ -64,16 +68,21 @@ while(<>){
       my $newSeqID = $2;
       my $newShortID = $3;
       my $len = length($seq);
-      my $ppb = ($size > $len) ? 1 : $size / $len;
+      my $logLen = ($len == 0) ? 1 : log($len);
+      my $ppb = ($size > $len) ? 1 : $size / $len; # pixels per base
+      my $ppl = $size / $logLen; # pixels per log
       my $sseq = "";
       if($seqID && (length($seq) > $kmerLength)){
         if($subseq){
           $seq = substr($seq, $region[0], ($region[1]-$region[0]));
           $len = length($seq);
-          $ppb = ($size > $len) ? 1 : $size / $len;
+	  $logLen = ($len == 0) ? 0 : log($len);
+	  $ppb = ($size > $len) ? 1 : $size / $len;
+	  $ppl = $size / $logLen;
         }
         my $countTotal = 0;
         my $countMax = 0;
+	my $dist = 0;
         my $maxKmer = "";
 	my @rptCounts = ();
 	my %posHash = ();
@@ -85,19 +94,43 @@ while(<>){
 	  my @posList = @{$posHash{$kmer}};
           foreach my $x (@posList){
 	    foreach my $y (@posList){
-	      $im->setPixel($x * $ppb, $y * $ppb, $red);
+	      if($blockPicture){
+		if($x != $y){
+		  $dist = log(abs($x - $y));
+		  $im->setPixel($x * $ppb, $size - $dist * $ppl, $red);
+		  $im->setPixel($y * $ppb, $size - $dist * $ppl, $magenta);
+		}
+	      } else {
+		$im->setPixel($x * $ppb, $y * $ppb, $red);
+	      }
 	    }
           }
 	}
-	## make sure reverse and reverse complement explicityl overwrite
+	## make sure reverse and reverse complement explicitly overwrite
 	foreach my $kmer (keys(%posHash)){
 	  my @posList = @{$posHash{$kmer}};
           foreach my $x (@posList){
 	    foreach my $y (grep {$_ < $x} (@{$posHash{rc($kmer)}})){
-	      $im->setPixel($x * $ppb, $y * $ppb, $blue);
+	      if($blockPicture){
+		if($x != $y){
+		  $dist = log(abs($x - $y));
+		  $im->setPixel($x * $ppb, $size - $dist * $ppl, $blue);
+		  $im->setPixel($y * $ppb, $size - $dist * $ppl, $cyan);
+		}
+	      } else {
+		$im->setPixel($x * $ppb, $y * $ppb, $blue);
+	      }
 	    }
 	    foreach my $y (grep {$_ > $x} (@{$posHash{rev($kmer)}})){
-	      $im->setPixel($x * $ppb, $y * $ppb, $green);
+	      if($blockPicture){
+		if($x != $y){
+		  $dist = log(abs($x - $y));
+		  $im->setPixel($x * $ppb, $size - $dist * $ppl, $green);
+		  $im->setPixel($y * $ppb, $size - $dist * $ppl, $yellow);
+		}
+	      } else {
+		$im->setPixel($x * $ppb, $y * $ppb, $green);
+	      }
 	    }
           }
 	}
@@ -121,39 +154,69 @@ while(<>){
 }
 
 my $len = length($seq);
-my $ppb = ($size > $len) ? 1 : $size / $len;
-if ($seqID && (length($seq) > $kmerLength)) {
+my $logLen = ($len == 0) ? 1 : log($len);
+my $ppb = ($size > $len) ? 1 : $size / $len; # pixels per base
+my $ppl = $size / $logLen; # pixels per log
+my $sseq = "";
+if($seqID && (length($seq) > $kmerLength)){
   if($subseq){
     $seq = substr($seq, $region[0], ($region[1]-$region[0]));
     $len = length($seq);
+    $logLen = ($len == 0) ? 0 : log($len);
     $ppb = ($size > $len) ? 1 : $size / $len;
+    $ppl = $size / $logLen;
   }
   my $countTotal = 0;
   my $countMax = 0;
+  my $dist = 0;
   my $maxKmer = "";
   my @rptCounts = ();
   my %posHash = ();
   my %gapCounts = ();
-  for (my $p = 0; ($p + $kmerLength) <= $len; $p++) {
+  for(my $p = 0; ($p + $kmerLength) <= $len; $p++){
     push(@{$posHash{substr($seq, $p, $kmerLength)}}, $p);
   }
   foreach my $kmer (keys(%posHash)){
     my @posList = @{$posHash{$kmer}};
     foreach my $x (@posList){
       foreach my $y (@posList){
-	$im->setPixel($x * $ppb, $y * $ppb, $red);
+	if($blockPicture){
+	  if($x != $y){
+	    $dist = log(abs($x - $y));
+	    $im->setPixel($x * $ppb, $size - $dist * $ppl, $red);
+	    $im->setPixel($y * $ppb, $size - $dist * $ppl, $magenta);
+	  }
+	} else {
+	  $im->setPixel($x * $ppb, $y * $ppb, $red);
+	}
       }
     }
   }
-  ## make sure reverse and reverse complement explicityl overwrite
+  ## make sure reverse and reverse complement explicitly overwrite
   foreach my $kmer (keys(%posHash)){
     my @posList = @{$posHash{$kmer}};
     foreach my $x (@posList){
       foreach my $y (grep {$_ < $x} (@{$posHash{rc($kmer)}})){
-	$im->setPixel($x * $ppb, $y * $ppb, $blue);
+	if($blockPicture){
+	  if($x != $y){
+	    $dist = log(abs($x - $y));
+	    $im->setPixel($x * $ppb, $size - $dist * $ppl, $blue);
+	    $im->setPixel($y * $ppb, $size - $dist * $ppl, $cyan);
+	  }
+	} else {
+	  $im->setPixel($x * $ppb, $y * $ppb, $blue);
+	}
       }
       foreach my $y (grep {$_ > $x} (@{$posHash{rev($kmer)}})){
-	$im->setPixel($x * $ppb, $y * $ppb, $green);
+	if($blockPicture){
+	  if($x != $y){
+	    $dist = log(abs($x - $y));
+	    $im->setPixel($x * $ppb, $size - $dist * $ppl, $green);
+	    $im->setPixel($y * $ppb, $size - $dist * $ppl, $yellow);
+	  }
+	} else {
+	  $im->setPixel($x * $ppb, $y * $ppb, $green);
+	}
       }
     }
   }
