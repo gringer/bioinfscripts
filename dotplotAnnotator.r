f1da956898bc4d3425ea67dfad7633b3d7ccb5d1
David Eccles (gringer) <bioinformatics@gringene.org>
2018-02-01 00:50:45 +1300

    change to "laptop" version of annotation code

--- dotplotAnnotator.r
+++ dotplotAnnotator.r
@@ -1,5 +1,10 @@
 #!/usr/bin/Rscript
 
+library(png);
+
+fname <- "0013560_tig00022845.png";
+img <- readPNG(paste0("rippleplots/",fname));
+
 valToSci <- function(val, unit = ""){
     sci.prefixes <- c("", "k", "M", "G", "T", "P", "E", "Z", "Y");
     units <- rep(paste(sci.prefixes,unit,sep=""), each=3);
@@ -9,31 +14,33 @@ valToSci <- function(val, unit = ""){
     conv.val <- val / conv.div;
     conv.val[val == 0] <- 0;
     conv.units[val == 0] <- unit;
-    return(ifelse(conv.units == "", conv.val,
-                  sprintf("%s %s",conv.val,conv.units)));
+    return(sprintf("%s %s",conv.val,conv.units));
 }
 
-library(png);
-img.alt <- readPNG("alt_tig7744.png");
-seqLen <- 119722;
+fib.divs <- round(10^((0:4)/5) * 2) * 0.5; ## splits log decades into 5
 
-png("tig7744_thinkable.png", width=1280, height=720, pointsize=16);
-par(mar=c(4.5,5.5,0.5,0.5));
-plot(NA, xlim=c(1,seqLen/1000), ylim=c(1,seqLen),
-     log="y", ann=FALSE, axes=FALSE,
-     frame.plot=TRUE);
-rasterImage(img.alt, xleft=1, xright=seqLen/1000, ybottom=1, ytop=seqLen);
-legend("bottom", horiz=TRUE, inset=0.025, cex=1.2,
-       legend=c("Repeat(L)","Repeat(R)",
-                "Reverse(L)","Reverse(R)","RevComp(L)","RevComp(R)"),
-       fill=c("#9000a0","#8b0000","#00a090","#0000ff","#a09000","#00a000"));
-mtext("tig7744 location (kbp)", side=1, line=3, cex=1.5);
-mtext("Feature distance (bp)", side=2, line=3.5, cex=1.5);
-axis(1, cex.axis=1.5, lwd=3);
+seqName <- sub("^.*_(.*?).png","\\1",fname);
+seqLen <- as.numeric(sub("_.*$","",fname));
+
+png(sprintf("featurePlot_%s.png", seqName),
+    width=1280, height=720, pointsize=16);
+par(mar=c(5,6,3,1.5), cex.axis=1.5, cex.lab=1.5, cex.main=2);
+plot(NA, main=sprintf("Feature profile (%s)", seqName),
+     xlab="Sequence Location (kbp)",
+     ylab="", log="y", xlim=c(0,77263)/1000,
+     ylim=c(1,77263), yaxt="n");
+rasterImage(img, xleft=1, xright=77263/1000,
+            ybottom=1, ytop=77263);
 drMax <- ceiling(log10(seqLen));
-axis(2, at= 10^(0:drMax), lwd=3, cex.axis=1.5, labels=valToSci(10^(0:drMax)),
-     las=2);
+axis(2, at= 10^(0:drMax), las=2, lwd=3, cex.axis=1.5, labels=valToSci(10^(0:drMax)));
 axis(2, at= rep(1:9, each=drMax+1) * 10^(0:drMax), labels=FALSE);
-abline(h=10^(1:drMax), col="#80808040",
-       lwd=3);
+abline(h=10^(0:drMax), col="#80808050", lwd = 3);
+mtext("Feature distance (bp)", 2, line=4, cex=1.5);
+legend(x = "bottom",
+    fill=c("#9000a0","#8b0000","#00a090","#0000FF","#a09000","#00A000"),
+    legend=c("Repeat (L)",  "Repeat (R)",
+             "RevComp (L)", "RevComp (R)",
+             "Reverse (L)", "Reverse (R)"),
+    bg="#FFFFFF",
+    horiz=TRUE, inset=0.01);
 invisible(dev.off());
