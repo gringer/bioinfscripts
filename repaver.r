7723bc140546bf284a255a5fecc96335fe87b416
David Eccles (gringer) <bioinformatics@gringene.org>
2018-05-05 02:53:34 +1200

    Remove an unnecessary python import

--- repaver.r
+++ repaver.r
@@ -1,6 +1,5 @@
 #!/usr/bin/Rscript
-
-## repaver.r - Repetitive pattern visualiser for extremely long reads
+## repaver.r - REpetitive PAttern Visualiser for Extremely-long Reads
 
 library(reticulate);
 
@@ -12,13 +11,13 @@ dnaSeqFile <- if(length(commandArgs(TRUE) > 0)){
 py_run_string("
 from Bio import SeqIO
 from collections import defaultdict
-from itertools import groupby
 def getKmerLocs(seqFile, kSize=17):
    fileKmers = dict()
    for record in SeqIO.parse(seqFile, \"fasta\"):
       kmers = defaultdict(list)
       seq = str(record.seq)
-      for k, v in zip([seq[d:d+kSize] for d in xrange(len(seq)-kSize+1)], xrange(len(seq)-kSize+1)):
+      for k, v in zip([seq[d:d+kSize] for d in
+            xrange(len(seq)-kSize+1)], xrange(len(seq)-kSize+1)):
          kmers[k].append(v)
       fileKmers[record.id] = kmers
    return(fileKmers)
