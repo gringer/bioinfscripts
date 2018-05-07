a10cded60324e5e0df8a74695aafe4eed72b8158
David Eccles (gringer) <github@gringer.org>
2018-05-07 18:07:58 +1200

    Optimising kmer collection portion of script

--- repaver.r
+++ repaver.r
@@ -1,14 +1,11 @@
 #!/usr/bin/Rscript
 ## repaver.r - REpetitive PAttern Visualiser for Extremely-long Reads
 
-library(reticulate);
-
 outputStyle <- "circular"; ## dotplot, circular, logplot
+outputType <- "png";
 kmerLength <- 17;
 
-dnaSeqFile <- if(length(commandArgs(TRUE) > 0)){
-                  commandArgs(TRUE)[1];
-              } else "data/circ-Nb-ec3-mtDNA.fasta";
+dnaSeqFile <- "data/circ-Nb-ec3-mtDNA.fasta";
 
 ## helper R functions
 fib.divs <- round(10^((0:4)/5) * 2) * 0.5; ## splits log decades into 5
@@ -24,6 +21,51 @@ valToSci <- function(val, unit = ""){
     conv.units[val == 0] <- unit;
     return(sprintf("%s %s",conv.val,conv.units));
 }
+
+usage <- function(){
+  cat("usage: ./repaver.r",
+      "<fasta/fastq file> [options]\n");
+  cat("\nOther Options:\n");
+  cat("-help          : Only display this help message\n");
+  cat("-k <int>       : Set kmer length\n");
+  cat("-type <string> : Output file type (png|svg)\n");
+  cat("\n");
+}
+
+argLoc <- 1;
+
+if(length(commandArgs(TRUE)) < 1){
+      usage();
+      quit(save = "no", status=0);
+}
+
+while(!is.na(commandArgs(TRUE)[argLoc])){
+  if(file.exists(commandArgs(TRUE)[argLoc])){ # file existence check
+      dnaSeqFile <- commandArgs(TRUE)[argLoc];
+  } else {
+    if(commandArgs(TRUE)[argLoc] == "-help"){
+      usage();
+      quit(save = "no", status=0);
+    }
+    else if(commandArgs(TRUE)[argLoc] == "-k"){
+      kmerLength <- as.numeric(commandArgs(TRUE)[argLoc+1]);
+      argLoc <- argLoc + 1;
+    }
+    else if(commandArgs(TRUE)[argLoc] == "-type"){
+      outputType <- commandArgs(TRUE)[argLoc+1];
+      argLoc <- argLoc + 1;
+    }
+    else {
+      cat("Error: Argument '",commandArgs()[argLoc],
+          "' is not understood by this program\n\n", sep="");
+      usage();
+      quit(save = "no", status=0);
+    }
+  }
+  argLoc <- argLoc + 1;
+}
+
+library(reticulate);
 
 ## Create python function to quickly generate a kmer location
 ## dictionary, then filter out unique kmers
@@ -53,8 +95,17 @@ def getKmerLocs(seqFile, kSize=17):
            (k[::-1] in kmers) or       ## reverse
            (comp(k) in kmers) or       ## complement
            (comp(k[::-1]) in kmers)))} ## reverse complement
-      kmers['length'] = len(seq) ## add in length, because it's cheap
-      fileKmers[record.id] = kmers
+      kmersFwd = {k:v for k, v in kmers.iteritems() if (
+           (len(v) > 1))}
+      kmersRev = {k:v for k, v in kmers.iteritems() if (
+           ((k[::-1] in kmers)))}
+      kmersComp = {k:v for k, v in kmers.iteritems() if (
+           ((comp(k) in kmers)))}
+      kmersRevComp = {k:v for k, v in kmers.iteritems() if (
+           ((comp(k[::-1]) in kmers)))}
+      seqLen = len(seq) ## add in length, because it's cheap
+      fileKmers[record.id] = dict({'len':seqLen, 'fwd':kmersFwd, 'rev':kmersRev,
+                                  'comp':kmersComp, 'rc':kmersRevComp})
    return(fileKmers)
 ");
 
@@ -69,11 +120,19 @@ cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time, attr(Sys.time() - my.ti
 for(dnaSeqMapName in names(res)){
     dnaSeqMap <- res[[dnaSeqMapName]];
     if(outputStyle == "dotplot"){
-        png(width=1920, height=1080, pointsize=18, antialias="gray");
+        if(outputType == "svg"){
+            svg(width=20, height=11.25, pointsize=18);
+        } else {
+            png(width=1920, height=1080, pointsize=18, antialias="gray");
+        }
     } else {
-        png(width=1080, height=1080, pointsize=18, antialias="gray");
+        if(outputType == "svg"){
+            svg(width=11.25, height=11.25, pointsize=18);
+        } else {
+            png(width=1080, height=1080, pointsize=18, antialias="gray");
+        }
     }
-    sLen <- dnaSeqMap$length;
+    sLen <- dnaSeqMap$len;
     if(outputStyle == "dotplot"){
         par(mgp=c(2,0.5,0));
         plot(NA, xlim=c(0,sLen), ylim=c(sLen,0),
@@ -114,72 +173,78 @@ for(dnaSeqMapName in names(res)){
              axes=FALSE, xlab="", ylab="",
              main=sprintf("%s (k=%d)", dnaSeqMapName, kmerLength));
     }
-    my.time <- Sys.time();
-    cat("Deriving kmer names... ");
-    revNames <- sapply(names(dnaSeqMap), py$rev);
-    revCNames <- sapply(names(dnaSeqMap), py$rc);
-    compNames <- sapply(names(dnaSeqMap), py$comp);
-    cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time,
-                attr(Sys.time() - my.time, "units")));
-    my.time <- Sys.time();
-    cat("Determining kmer validity... repeated...");
-    repeatedKmers <- sapply(dnaSeqMap, function(x){length(x) > 1});
-    cat(" reverse complement...");
-    rcKmers <- revCNames %in% names(dnaSeqMap);
-    cat(" reverse...");
-    rKmers <- revNames %in% names(dnaSeqMap);
-    cat(" complement...");
-    cKmers <- compNames %in% names(dnaSeqMap);
-    cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time,
-                attr(Sys.time() - my.time, "units")));
     ## f,c,rc,r : red, orange, blue, green
-    plotPointsF <- NULL;
-    plotPointsC <- NULL;
-    plotPointsRC <- NULL;
-    plotPointsR <- NULL;
+    plotPoints <- NULL;
     my.time <- Sys.time();
     cat("Processing repeats... ");
-    plotPointsF <-
-        Reduce(rbind,sapply(dnaSeqMap[repeatedKmers],
-                            function(kposs){
-                                data.frame(x=rep(kposs, length(kposs)),
-                                           y=rep(kposs, each=length(kposs)))
-                            }, simplify=FALSE),NULL);
+    if(length(dnaSeqMap$fwd) > 0){
+        plotPoints <-
+            rbind(plotPoints,
+                  Reduce(rbind,sapply(dnaSeqMap$fwd,
+                                      function(kposs){
+                                          data.frame(x=rep(kposs, length(kposs)),
+                                                     y=rep(kposs, each=length(kposs)),
+                                                     type=rep("F",length(kposs)))
+                                      }, simplify=FALSE),NULL)
+                  );
+    }
     cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time,
                 attr(Sys.time() - my.time, "units")));
     my.time <- Sys.time();
     cat("Processing complements... ");
-    for(kmer in names(dnaSeqMap)[cKmers]){
-        kposs <- dnaSeqMap[[kmer]];
-        oposs <- dnaSeqMap[[py$comp(kmer)]];
-        plotPointsC <- rbind(plotPointsC,
-                            data.frame(x=rep(kposs, length(oposs)),
-                                       y=rep(oposs, each=length(kposs))));
+    if(length(dnaSeqMap$comp) > 0){
+        plotPoints <-
+            rbind(plotPoints,
+                  Reduce(rbind,sapply(names(dnaSeqMap$comp),
+                                      function(kmer){
+                                          kposs <- dnaSeqMap$comp[[kmer]];
+                                          oposs <- dnaSeqMap$comp[[py$comp(kmer)]];
+                                          data.frame(x=rep(kposs, length(oposs)),
+                                                     y=rep(oposs, each=length(kposs)),
+                                                     type=rep("C",length(kposs)))
+                                      }, simplify=FALSE),NULL)
+                  );
     }
     cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time, attr(Sys.time() - my.time, "units")));
     my.time <- Sys.time();
     cat("Processing reverse complements... ");
-    for(kmer in names(dnaSeqMap)[rcKmers]){
-        kposs <- dnaSeqMap[[kmer]];
-        oposs <- dnaSeqMap[[py$rc(kmer)]];
-        plotPointsRC <- rbind(plotPointsRC,
-                            data.frame(x=rep(kposs, length(oposs)),
-                                       y=rep(oposs, each=length(kposs))));
+    if(length(dnaSeqMap$rc) > 0){
+        plotPoints <-
+            rbind(plotPoints,
+                  Reduce(rbind,sapply(names(dnaSeqMap$rc),
+                                      function(kmer){
+                                          kposs <- dnaSeqMap$rc[[kmer]];
+                                          oposs <- dnaSeqMap$rc[[py$comp(kmer)]];
+                                          data.frame(x=rep(kposs, length(oposs)),
+                                                     y=rep(oposs, each=length(kposs)),
+                                                     type=rep("RC",length(kposs)))
+                                      }, simplify=FALSE),NULL)
+                  );
     }
     cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time,
                 attr(Sys.time() - my.time, "units")));
     my.time <- Sys.time();
     cat("Processing reverses... ");
-    for(kmer in names(dnaSeqMap)[rKmers]){
-        kposs <- dnaSeqMap[[kmer]];
-        oposs <- dnaSeqMap[[py$rev(kmer)]];
-        plotPointsR <- rbind(plotPointsR,
-                            data.frame(x=rep(kposs, length(oposs)),
-                                       y=rep(oposs, each=length(kposs))));
+    if(length(dnaSeqMap$rev) > 0){
+        plotPoints <-
+            rbind(plotPoints,
+                  Reduce(rbind,sapply(names(dnaSeqMap$rev),
+                                      function(kmer){
+                                          kposs <- dnaSeqMap$rev[[kmer]];
+                                          oposs <- dnaSeqMap$rev[[py$comp(kmer)]];
+                                          data.frame(x=rep(kposs, length(oposs)),
+                                                     y=rep(oposs, each=length(kposs)),
+                                                     type=rep("R",length(kposs)))
+                                      }, simplify=FALSE),NULL)
+                  );
     }
     cat(sprintf(" done in %0.2f %s\n", Sys.time() - my.time,
                 attr(Sys.time() - my.time, "units")));
     my.time <- Sys.time();
+    print(str(plotPointsF));
+    print(str(plotPointsC));
+    print(str(plotPointsRC));
+    print(str(plotPointsR));
     cat("Drawing plot... ");
     if(outputStyle == "dotplot"){
         points(plotPointsF, pch=15, col="#8b000040", cex=0.5);
@@ -199,6 +264,12 @@ for(dnaSeqMapName in names(res)){
         plotPointsC <-  subset(plotPointsC, dist > 0);
         plotPointsRC <- subset(plotPointsRC, dist > 0);
         plotPointsR <-  subset(plotPointsR, dist > 0);
+        plotPoints <- NULL;
+        plotPoints <- rbind(plotPoints,
+                            data.frame(plotPointsF, type="f"),
+                            data.frame(plotPointsC, type="c"),
+                            data.frame(plotPointsRC, type="rc"),
+                            data.frame(plotPointsR, type="r"));
         points(plotPointsF$x,  plotPointsF$dist,
                pch=15, col="#8b000040", cex=0.5); # red
         points(plotPointsC$x,  plotPointsC$dist,
@@ -322,9 +393,9 @@ for(dnaSeqMapName in names(res)){
                      },
                  col="black");
         }
-        points(x=0.2*cos(seq(10^(drMax-2)/sLen * 2*pi / 5,
+        points(x=0.2*cos(seq(10^(drMax-2)/sLen * 2*pi/2,
                              2*pi, length.out=360)),
-               y=0.2*sin(seq(10^(drMax-2)/sLen * 2*pi / 5,
+               y=0.2*sin(seq(10^(drMax-2)/sLen * 2*pi/2,
                              2*pi, length.out=360)),
                type="l", lwd=3, col="#000000A0"); # tick circle
         points(plotPointsF$r*cos(plotPointsF$x/sLen*2*pi),
@@ -361,7 +432,7 @@ for(dnaSeqMapName in names(res)){
                y0=0, angle=90, code=3, length=0.15, lwd=3, col="#00000080");
         text(x=pwFun(scalePtsMajor), y=0, col="black",
              labels=valToSci(signif(scalePtsMajor,2)), pos=1, offset=1,
-             cex=0.75);
+             cex=ifelse(length(scalePtsMajor) > 5, 0.5, 0.75));
         text(x=mean(range(pwFun(scalePtsMajor))), y=0, col="black",
              labels="Feature Distance (bases)", pos=3, offset=1, cex=0.75);
         text(x=0, y=0, labels="Sequence\nLocation\n(bases)", col="black",
