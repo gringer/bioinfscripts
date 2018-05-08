6d0168567bdc7e8ed5440b5fc11a5d6d69d811fc
David Eccles (gringer) <github@gringer.org>
2018-05-08 18:32:22 +1200

    Add in semicircular display

--- repaver.r
+++ repaver.r
@@ -1,9 +1,10 @@
 #!/usr/bin/Rscript
 ## repaver.r - REpetitive PAttern Visualiser for Extremely-long Reads
 
-outputStyle <- "circular"; ## dotplot, circular, profile
+outputStyle <- "semicircular"; ## dotplot, circular, semicircular, profile
 outputType <- "png";
 kmerLength <- 17;
+filePrefix <- "repaver";
 
 dnaSeqFile <- "data/circ-Nb-ec3-mtDNA.fasta";
 
@@ -26,10 +27,11 @@ usage <- function(){
   cat("usage: ./repaver.r",
       "<fasta/fastq file> [options]\n");
   cat("\nOther Options:\n");
-  cat("-help           : Only display this help message\n");
-  cat("-k <int>        : Set kmer length\n");
-  cat("-style <string> : Output file style (dotplot|profile|circular)\n");
-  cat("-type <string>  : Output file type (png|svg)\n");
+  cat("-help            : Only display this help message\n");
+  cat("-k <int>         : Set kmer length\n");
+  cat("-style <string>  : Output file style (dotplot|profile|circular|semicircular)\n");
+  cat("-type <string>   : Output file type (png|svg)\n");
+  cat("-prefix <string> : Output file prefix (default: 'repaver')\n");
   cat("\n");
 }
 
@@ -60,6 +62,10 @@ while(!is.na(commandArgs(TRUE)[argLoc])){
       outputStyle <- commandArgs(TRUE)[argLoc+1];
       argLoc <- argLoc + 1;
     }
+    else if(commandArgs(TRUE)[argLoc] == "-prefix"){
+      filePrefix <- commandArgs(TRUE)[argLoc+1];
+      argLoc <- argLoc + 1;
+    }
     else {
       cat("Error: Argument '",commandArgs()[argLoc],
           "' is not understood by this program\n\n", sep="");
@@ -70,10 +76,18 @@ while(!is.na(commandArgs(TRUE)[argLoc])){
   argLoc <- argLoc + 1;
 }
 
+if(!(outputStyle %in% c("dotplot","profile","circular","semicircular"))){
+    cat("Error: Plot style should be one of '(dotplot|profile|circular|semicircular)'\n");
+    usage();
+    quit(save = "no", status=0);
+}
+
 library(reticulate);
 
 ## Create python function to quickly generate a kmer location
-## dictionary, then filter out unique kmers
+## dictionary and determine the distance from kmers that have been seen before
+## This is chunked into no more than ~5000 pieces so that the final image
+## generation is slightly quicker.
 py_run_string("
 from string import maketrans
 from Bio import SeqIO
@@ -179,7 +193,7 @@ for(dnaSeqMapName in names(res)){
         axis(2, at= rep(1:9, each=drMax+1) * 10^(0:drMax), labels=FALSE);
         abline(h=10^(0:drMax), col="#80808050", lwd = 3);
         mtext("Feature distance (bp)", 2, line=4.5, cex=1.5);
-    } else if(outputStyle == "circular"){
+    } else if(outputStyle %in% c("circular","semicircular")){
         par(mgp=c(2.5,1,0), mar=c(2.5,2,1.5,2),
             cex.axis=1.5, cex.lab=1.5, cex.main=2);
         plot(NA, xlim=c(-1.1,1.1), ylim=c(-1.2,1),
@@ -216,21 +230,22 @@ for(dnaSeqMapName in names(res)){
     my.time <- Sys.time();
     cat("Drawing plot... ");
     if(outputStyle == "dotplot"){
-        points(plotPoints, pch=15, col=c(F="#8b000040",C="#FF7F0040",
-                                         RC="#0000FF40",R="#00A00040")[plotPoints$type], cex=0.5);
+        points(plotPoints, pch=15,
+               col=c(F="#8b000040",C="#FF7F0040",
+                     RC="#0000FF40",R="#00A00040")[plotPoints$type], cex=0.5);
         legend("bottomleft",
                legend=c("Forward","Complement","RevComp","Reverse"),
                fill=c("#8b000040","#FF7F0040","#0000FF40","#00A00040"),
                bg="#FFFFFFE0", inset=0.05);
     } else if(outputStyle == "profile"){
-        plotPoints$dist <-  plotPoints$y -  plotPoints$x;
-        plotPoints <-  subset(plotPoints, dist > 0);
         ## left symbols
-        points(x=plotPoints$x, y=plotPoints$dist, pch=15, col=c(F="#8b000040",C="#FDC08640",
-                                         RC="#0000FF40",R="#00A00040")[plotPoints$type], cex=0.5);
+        points(x=plotPoints$x, y=plotPoints$dist, pch=15,
+               col=c(F="#8b000040",C="#FDC08640",
+                     RC="#0000FF40",R="#00A00040")[plotPoints$type], cex=0.5);
         ## right symbols
-        points(x=plotPoints$y, y=plotPoints$dist, pch=15, col=c(F="#9000A040",C="#FF7F0040",
-                                         RC="#00A09040",R="#A0900040")[plotPoints$type], cex=0.5);
+        points(x=plotPoints$y, y=plotPoints$dist, pch=15,
+               col=c(F="#9000A040",C="#FF7F0040",
+                     RC="#00A09040",R="#A0900040")[plotPoints$type], cex=0.5);
         legend(x = "bottom",
                fill=c("#9000a0","#8b0000",
                       "#fdc086","#ff7f00",
@@ -305,17 +320,13 @@ for(dnaSeqMapName in names(res)){
                              2*pi, length.out=360)),
                type="l", lwd=3, col="#000000A0"); # tick circle
         ## left symbols
-        points(x=plotPoints$x, y=plotPoints$dist, pch=15, col=c(F="#8b000040",C="#FDC08640",
-                                         RC="#0000FF40",R="#00A00040")[plotPoints$type], cex=0.5);
-        ## right symbols
-        points(x=plotPoints$y, y=plotPoints$dist, pch=15, col=c(F="#9000A040",C="#FF7F0040",
-                                         RC="#00A09040",R="#A0900040")[plotPoints$type], cex=0.5);
         points(plotPoints$r*cos(plotPoints$x/sLen*2*pi),
                plotPoints$r*sin(plotPoints$x/sLen*2*pi),
                pch=ifelse(outputType=="png",20,"•"),
                col=c(F="#8b000040",C="#FDC08640",
                      RC="#0000FF40",R="#00A00040")[plotPoints$type],
                cex=ifelse(outputType=="png",0.5,1));
+        ## right symbols
         points(plotPoints$r*cos(plotPoints$y/sLen*2*pi),
                plotPoints$r*sin(plotPoints$y/sLen*2*pi),
                pch=ifelse(outputType=="png",20,"•"),
@@ -335,6 +346,113 @@ for(dnaSeqMapName in names(res)){
              cex=0.5);
         text(x=mean(range(pwFun(scalePtsMajor))), y=0, col="black",
              labels="Feature Distance (bases)", pos=3, offset=1, cex=0.75);
+        text(x=0, y=0, labels="Sequence\nLocation\n(bases)", col="black",
+             cex=0.75);
+        legend(x = "bottom",
+               fill=c("#9000a0","#8b0000",
+                      "#fdc086","#ff7f00",
+                      "#00a090","#0000ff",
+                      "#a09000","#00a000"),
+               legend=c("Repeat (L)",  "Repeat (R)",
+                        "Comp (L)",    "Comp (R)",
+                        "RevComp (L)", "RevComp (R)",
+                        "Reverse (L)", "Reverse (R)"),
+               bg="#FFFFFFE0", horiz=FALSE, inset=0.01, ncol=4);
+    } else if(outputStyle == "semicircular"){
+        ## Convert distance to radius. This is a piecewise function
+        ## with the following properties:
+        ## * Starts off as a log function
+        ## * Remainder is a linear function
+        ## * The transition point is the point where the slope is equal
+        ## * The transition point is 1/3 along the radius
+        ## * The plot ends at (sLen/2, 1)
+        ## * The base of the log is sLen/12
+        ## Note: slope of log[b](x) = 1/(x*log(b))
+        pwFun <- function(d){
+            a <- sLen/25; ## changes to linear approximately 1/3 of the way in
+            aProp <- sLen / a;
+            ifelse(d < a,
+                   log(d) / log(a),
+                   d/(a * log(a)) + (1 - 1/log(a))) /
+                ((aProp-1) / log(a) + 1) * 0.75 + 0.25;
+        }
+        cat("converting distances... ");
+        plotPoints$r <- pwFun(plotPoints$dist);
+        cat("drawing points... ");
+        drMax <- ceiling(log10(sLen));
+        scalePtsMajor <- rep(1, each=drMax+1) * 10^(0:drMax);
+        scalePtsMajor <- c(scalePtsMajor[scalePtsMajor < sLen], sLen);
+        scalePts <- rep(1:9, each=drMax+1) * 10^(0:drMax);
+        scalePts <- scalePts[scalePts <= sLen];
+        for(p in scalePtsMajor){ # rings for log scale
+            points(x=pwFun(p)*cos(seq(0,pi, length.out=180)),
+                   y=pwFun(p)*sin(seq(0,pi, length.out=180)),
+                   type="l", lwd=3, col="#808080A0");
+        }
+        distPts <- (0:99)*10^(drMax-2);
+        distPts <- c(head(distPts[distPts < sLen], -1), sLen);
+        if(length(distPts) > 20){
+            distPts <- (1:9)*10^(drMax-1);
+            distPts <- signif(c(distPts[distPts < sLen], sLen),3);
+        }
+        segments(x0=-0.18*cos(distPts / sLen * pi), # tick marks on base location circle
+                 x1=-0.2*cos(distPts / sLen * pi),
+                 y0=0.18*sin(distPts / sLen * pi),
+                 y1=0.2*sin(distPts / sLen * pi),
+                 lwd=2, col="#000000A0");
+        for(dpi in seq_along(distPts)){ # tick labels for base location
+            text(x=-0.14*cos(distPts[dpi] / sLen * pi),
+                 y=0.14*sin(distPts[dpi] / sLen * pi),
+                 labels=valToSci(signif(distPts[dpi],3)), cex=0.5,
+                 srt=-if((distPts[dpi]/sLen * 180 >= 90) &&
+                        (distPts[dpi]/sLen * 180 < 270)){
+                         (distPts[dpi]/sLen * 180 + 180);
+                     } else {
+                         (distPts[dpi]/sLen * 180);
+                     },
+                 col="black");
+        }
+        points(x=0.2*cos(seq(0, pi, length.out=180)),
+               y=0.2*sin(seq(0, pi, length.out=180)),
+               type="l", lwd=3, col="#000000A0"); # tick circle
+        ## left symbols
+        points(-plotPoints$r*cos(plotPoints$x/sLen*pi),
+               plotPoints$r*sin(plotPoints$x/sLen*pi),
+               pch=ifelse(outputType=="png",20,"•"),
+               col=c(F="#8b000040",C="#FDC08640",
+                     RC="#0000FF40",R="#00A00040")[plotPoints$type],
+               cex=ifelse(outputType=="png",0.5,1));
+        ## right symbols
+        points(-plotPoints$r*cos(plotPoints$y/sLen*pi),
+               plotPoints$r*sin(plotPoints$y/sLen*pi),
+               pch=ifelse(outputType=="png",20,"•"),
+               col=c(F="#9000A040",C="#FF7F0040",
+                     RC="#00A09040",R="#A0900040")[plotPoints$type],
+               cex=ifelse(outputType=="png",0.5,1));
+        ## tick marks (left)
+        arrows(x0=-pwFun(head(scalePts,-1)),
+               x1=-pwFun(tail(scalePts,-1)),
+               y0=0, angle=90, code=3, length=0.1, lwd=2, col="#80808080");
+        arrows(x0=-pwFun(head(scalePtsMajor,-1)),
+               x1=-pwFun(tail(scalePtsMajor,-1)),
+               y0=0, angle=90, code=3, length=0.15, lwd=3, col="#00000080");
+        ## tick marks (right)
+        arrows(x0=pwFun(head(scalePts,-1)),
+               x1=pwFun(tail(scalePts,-1)),
+               y0=0, angle=90, code=3, length=0.1, lwd=2, col="#80808080");
+        arrows(x0=pwFun(head(scalePtsMajor,-1)),
+               x1=pwFun(tail(scalePtsMajor,-1)),
+               y0=0, angle=90, code=3, length=0.15, lwd=3, col="#00000080");
+        text(x=pwFun(scalePtsMajor), y=0, col="black",
+             labels=valToSci(signif(scalePtsMajor,2)), pos=1, offset=1,
+             cex=0.5); # tick labels for distance axis
+        text(x=-pwFun(scalePtsMajor), y=0, col="black",
+             labels=valToSci(signif(scalePtsMajor,2)), pos=1, offset=1,
+             cex=0.5); # tick labels for distance axis
+        text(x=-mean(range(pwFun(scalePtsMajor))), y=-0.05, col="black",
+             labels="Feature Distance (bases)", pos=1, offset=1, cex=0.75);
+        text(x=mean(range(pwFun(scalePtsMajor))), y=-0.05, col="black",
+             labels="Feature Distance (bases)", pos=1, offset=1, cex=0.75);
         text(x=0, y=0, labels="Sequence\nLocation\n(bases)", col="black",
              cex=0.75);
         legend(x = "bottom",
